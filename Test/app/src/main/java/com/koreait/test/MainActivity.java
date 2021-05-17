c6398dbaa4508196eca31cc41f1dfd6e8438a18b
DESKTOP-NUOPUIF\ITPS <yky2496@nave.com>
2021-05-17 17:40:07 +0900

    이제까지 안드로이드 스튜디오

---
+++ Test/app/src/main/java/com/koreait/test/MainActivity.java
@@ -0,0 +1,44 @@
+package com.koreait.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String hello = new String("안녕하세요");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
